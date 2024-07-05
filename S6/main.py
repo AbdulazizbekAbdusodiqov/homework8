aeb628e5caa51c5895b0dc31299d5a7f327da122
AbuBackSeat <abdulazizbekabdusodiqov@gmail.com>
2024-07-05 14:57:25 +0500

    1-vazifa bajarildi

---
+++ S6/main.py
@@ -0,0 +1,13 @@
+def split_separate_line(lst):
+    for i in range(len(lst)-1): #indexerror bolmasligi uchun
+        if lst[i] == lst[i+1]-1 or lst[i] == lst[i+1]+1:
+            print(lst[i], lst[i+1])
+        elif lst[i] < 0: #manfiy sonlar uchun
+            if lst[i] == lst[i+1]+1 or lst[i] == lst[i+1]-1:
+                print(lst[i], lst[i+1])
+
+
+n = int(input("n= "))
+lst = [int(input(f"{i+1}->")) for i in range(n)]
+
+split_separate_line(lst)
