9c7ef66b9ae2dae6aef032fcc581e43992171f68
AbuBackSeat <abdulazizbekabdusodiqov@gmail.com>
2024-07-05 15:18:11 +0500

    1-vazifa ortiqcha shartlari olib tashlandi

--- S6/main.py
+++ S6/main.py
@@ -1,11 +1,10 @@
 def split_separate_line(lst):
+    print("=================")
     for i in range(len(lst)-1): #indexerror bolmasligi uchun
         if lst[i] == lst[i+1]-1 or lst[i] == lst[i+1]+1:
             print(lst[i], lst[i+1])
-        elif lst[i] < 0: #manfiy sonlar uchun
-            if lst[i] == lst[i+1]+1 or lst[i] == lst[i+1]-1:
-                print(lst[i], lst[i+1])
 
+########################################################################
 
 n = int(input("n= "))
 lst = [int(input(f"{i+1}->")) for i in range(n)]
